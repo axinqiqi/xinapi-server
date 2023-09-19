e2f9edaa9e1f6e1fa5c185680577b5b1325a74e8
axinqiqi <you19970724@qq.com>
2023-09-19 14:38:38 +0800

    调试接口

--- xinapi-backend/src/main/java/com/xin/project/service/impl/UserServiceImpl.java
+++ xinapi-backend/src/main/java/com/xin/project/service/impl/UserServiceImpl.java
@@ -154,7 +154,7 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User>
         if (userAccount.length() < 4) {
             throw new BusinessException(ErrorCode.PARAMS_ERROR, "账号错误");
         }
-        if (userPassword.length() < 8) {
+        if (userPassword.length() < 4) {
             throw new BusinessException(ErrorCode.PARAMS_ERROR, "密码错误");
         }
         // 2. 加密
